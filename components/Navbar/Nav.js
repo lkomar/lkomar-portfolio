041884681e36099a749b398af101680fbaab4987
Łukasz Komar <lkomar@speednet.pl>
2021-12-11 21:14:48 +0100

    Update styles for landing page

--- components/Navbar/Nav.js
+++ components/Navbar/Nav.js
@@ -1,5 +1,5 @@
 const Nav = ({ children }) => (
-  <nav className="p-4">
+  <nav className="p-4 w-full">
     <ul className="flex space-x-2">{children}</ul>
   </nav>
 )
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,17 +1,29 @@
+import Head from 'next/head'
+
 import Nav from './Nav'
 import NavLink from './NavLink'
 
+export const navbarHeight = '20'
+
 const Navbar = () => (
-  <div className="border-b-2 border-gray-200">
-    <Nav>
-      <NavLink href="/" isActive>
-        Home
-      </NavLink>
-      <NavLink href="/projects">Projects</NavLink>
-      <NavLink href="/about">About</NavLink>
-      <NavLink href="/contact">Contact</NavLink>
-    </Nav>
-  </div>
+  <>
+    <Head>
+      <title>Łukasz Komar | Portfolio</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <nav
+      className={`border-b-2 border-gray-200 w-full fixed left-0 top-0 right-0 z-20 bg-gray-900 text-gray-300 flex items-center h-${navbarHeight} box-border`}
+    >
+      <Nav>
+        <NavLink href="/" isActive>
+          Home
+        </NavLink>
+        <NavLink href="/projects">Projects</NavLink>
+        <NavLink href="/about">About</NavLink>
+        <NavLink href="/contact">Contact</NavLink>
+      </Nav>
+    </nav>
+  </>
 )
 
 export default Navbar
